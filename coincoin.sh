3991b1ce5f1cf1dfb18f5654832cb3a30ae1e6e6
FatalCaca <simon.ballu@gmail.com>
2016-04-25 17:53:40 +0200

    some apt-get and py path

---
+++ coincoin.sh
@@ -0,0 +1,17 @@
+sudo apt-get --assume-yes install\
+    curl\
+    git\
+    chromium-browser\
+    gksu\
+    leafpad\
+    vlc\
+    clementine\
+    pinta\
+    wine\
+    playonlinux\
+    sendmail\
+    iptables-persistent\
+    python3-pip\
+    dropbox\
+    python-gpgme\
+    steam
